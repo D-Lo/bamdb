36ccc083ebdc057ef6e6e50f6f0c02cc7181ac21
evanbiederstedt <evanbiederstedt@users.noreply.github.com>
2017-12-02 15:25:15 -0500

    Update bamdb.c
    fixed spacing/tabs issues

--- src/bamdb.c
+++ src/bamdb.c
@@ -56,27 +56,26 @@ static void usage(FILE *fp){
 int 
 generate_index_wgs (char *input_file_name, char *output_file_name)
 {
-	samFile *input_file = 0;
+    samFile *input_file = 0;
 
-	if ((input_file = sam_open(input_file_name, "r")) == 0) {
-		fprintf(stderr, "Unable to open file %s\n", input_file_name);
-		return 1;
+    if ((input_file = sam_open(input_file_name, "r")) == 0) {
+        fprintf(stderr, "Unable to open file %s\n", input_file_name);
+        return 1;
 	}
-
-	return convert_to_lmdb_wgs(input_file, output_file_name);   // this should work for QNAME & BX
+        return convert_to_lmdb_wgs(input_file, output_file_name);   // this should work for QNAME & BX
 }
 
 int 
 generate_index_ss (char *input_file_name, char *output_file_name)
 {
-	samFile *input_file = 0;                           // these checks also exit in bam_lmdb.c  REMOVE?
+    samFile *input_file = 0;                           // these checks also exit in bam_lmdb.c  REMOVE?
 
-	if ((input_file = sam_open(input_file_name, "r")) == 0) {     // these checks also exit in bam_lmdb.c  REMOVE?
-		fprintf(stderr, "Unable to open file %s\n", input_file_name);
-		return 1;
-	}
+    if ((input_file = sam_open(input_file_name, "r")) == 0) {     // these checks also exit in bam_lmdb.c  REMOVE?
+        fprintf(stderr, "Unable to open file %s\n", input_file_name);
+        return 1;
+        }
 
-	return convert_to_lmdb_ss(input_file, output_file_name);   // this should WILL NOT work, must replace `convert_to_lmdb()` for QNAME, CB, UB
+    return convert_to_lmdb_ss(input_file, output_file_name);   // this should WILL NOT work, must replace `convert_to_lmdb()` for QNAME, CB, UB
 }
 
 
@@ -86,33 +85,31 @@ generate_index_ss (char *input_file_name, char *output_file_name)
 int 
 main(int argc, char *argv[]){
 
-	int rc = 0;
-	int c;
+    int rc = 0;
+    int c;
     
     int optional_tags = 0;
-	char tagArray[MAXTAGCOUNT][MAXTAGNAME+1];  // store arbitrary BAM tags into char array `tagArray`
+    char tagArray[MAXTAGCOUNT][MAXTAGNAME+1];  // store arbitrary BAM tags into char array `tagArray`
 
-	if (argc < 2) { usage(stderr); return 1; }
-	if (strcmp(argv[1], "help") == 0 || strcmp(argv[1], "--help") == 0){
-		usage(stdout); 
-		return 0;
-			if (row_set != NULL) {
-                            for (size_t j = 0; j < row_set->n_entries; ++j) {
-            	                print_sequence_row(row_set->rows[j]);
-                            }
-                            free_row_set(row_set);
-                        }
-                }
-	}
+    if (argc < 2) { usage(stderr); return 1; }
+    if (strcmp(argv[1], "help") == 0 || strcmp(argv[1], "--help") == 0){
+        usage(stdout); 
+	return 0;
+    if (row_set != NULL) {
+	for (size_t j = 0; j < row_set->n_entries; ++j) {
+        print_sequence_row(row_set->rows[j]);}
+                            free_row_set(row_set);}
+    }
+}
 
-	if (strcmp(argv[1], "index WGS") == 0 || strcmp(argv[1], "index --WGS") == 0)                         rc = generate_index_wgs(argc-1, argv+1);   // QNAME, BX
-	else if (strcmp(argv[1], "index single-cell") == 0 || strcmp(argv[1], "index --single-cell") == 0)    rc = generate_index_ss(argc-1, argv+1);    // QNAME, CB, UB
+    if (strcmp(argv[1], "index WGS") == 0 || strcmp(argv[1], "index --WGS") == 0)                         rc = generate_index_wgs(argc-1, argv+1);   // QNAME, BX
+    else if (strcmp(argv[1], "index single-cell") == 0 || strcmp(argv[1], "index --single-cell") == 0)    rc = generate_index_ss(argc-1, argv+1);    // QNAME, CB, UB
 
-	else if (strcmp(argv[1], "version") == 0 || strcmp(argv[1], "--version") == 0){
+    else if (strcmp(argv[1], "version") == 0 || strcmp(argv[1], "--version") == 0){
         printf(
 "bamdb %s\n"
 "Using htslib %s\n"
-               bamdb_version(), htslib_version());
+        bamdb_version(), htslib_version());
     }
     else {
         fprintf(stderr, "[main] unrecognized command '%s'\n", argv[1]);
@@ -122,23 +119,23 @@ main(int argc, char *argv[]){
     static const struct option loptions[] = {
 		{"tag", required_argument, NULL, 't'}, 
 		{NULL, 0, NULL, 0}
-	};   
+     };   
 
     
     while ( (c = getopt(argc, argv, "t:", loptions, NULL))>0 ) {   
-    	switch(c) {
-    		case 't':
-    		if (optarg == NULL){
-    			extra_tag = NULL;
+        switch(c) {
+    	    case 't':
+    	        if (optarg == NULL){
+    		    extra_tag = NULL;
     		} 
-    		else{
+    	        else{
     		    // strcpy(extra_tag, optarg); -- not for an array
-                snprintf(tryArray[optional_tags++], MAXTAGNAME, "%s", optarg);
+                    snprintf(tryArray[optional_tags++], MAXTAGNAME, "%s", optarg);
     		    break;
     		}
         }
     }
 
-	return rc;
+    return rc;
 }
 
