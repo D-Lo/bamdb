30c9ee95251458fc1551f93d4ed651e0390d882c
Aaron Dornbrand-Lo <dl.aaron@gmail.com>
2018-05-19 15:47:23 +0000

    Adopt google C++ code style

--- include/bam_lmdb.h
+++ include/bam_lmdb.h
@@ -7,17 +7,18 @@
 #include "bam_api.h"
 #include "htslib/sam.h"
 
-
 typedef struct indices {
-	bool includes_qname;
-	size_t num_key_indices; // Does not include qname index
-	char **key_indices;
+  bool includes_qname;
+  size_t num_key_indices;  // Does not include qname index
+  char **key_indices;
 } indices_t;
 
-int convert_to_lmdb(samFile *input_file, char *db_path, indices_t *target_indices);
+int convert_to_lmdb(samFile *input_file, char *db_path,
+                    indices_t *target_indices);
 
 /* Return number of offsets on success, -1 on failure */
-int get_offsets(offset_list_t *offset_list, const char *db_path, const char *index_name, const char *key);
+int get_offsets(offset_list_t *offset_list, const char *db_path,
+                const char *index_name, const char *key);
 
 /**
  * Get a list of the available indices in an existing lmdb database
@@ -26,6 +27,7 @@ indices_t *get_available_indices(const char *db_path);
 
 bool is_index_present(const char *db_path, const char *index_name);
 
-bam_row_set_t *get_bam_rows(const char *input_file_name, const char *db_path, const char *key_type, const char *key_value);
+bam_row_set_t *get_bam_rows(const char *input_file_name, const char *db_path,
+                            const char *key_type, const char *key_value);
 
 #endif
---
+++ include/bamdb_status.h
@@ -0,0 +1,6 @@
+
+
+/** Successful result */
+#define BAMDB_SUCCESS 0
+/** Error at the database level */
+#define BAMDB_DB_ERROR -16000
